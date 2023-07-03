e64409f56c3684d10fd86f99350365cc3330e693
Mohith B A <110193919+MOHITH-2002@users.noreply.github.com>
2023-07-03 22:16:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React from 'react'
 import Home from "./components/Home.jsx"
-import Login from "./components/Login.jsx"
-import Register from "./components/Register.jsx"
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+// import Login from "./components/Login.jsx"
+// import Register from "./components/Register.jsx"
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home/>,
-    },
-    {
-      path: "/login",
-      element: <Login/>,
-    },
-    {
-      path: "/register",
-      element: <Register/>,
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Home/>,
+  //   },
+  //   {
+  //     path: "/login",
+  //     element: <Login/>,
+  //   },
+  //   {
+  //     path: "/register",
+  //     element: <Register/>,
+  //   },
+  // ]);
   return (
     <div>
-         <RouterProvider router={router} />
+    <Home/>
+         // <RouterProvider router={router} />
     </div>
   )
 }
